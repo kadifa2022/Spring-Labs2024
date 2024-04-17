1b5167811a7ffe353f5c0389d5ae634f739e9358
Kadifa <Kudickadifa@gmail.com>
2024-04-16 22:05:09 -0500

    practice

--- Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Category.java
+++ Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Category.java
@@ -1,22 +1,19 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
-@Getter
 @Setter
-public class Customer extends BaseEntity{
+@Getter
+public class Category extends BaseEntity{
+
+    private String name;
 
 
 
-    private String firstName;
-    private String lastName;
-    private String userName;
-    private String email;
 
 }
