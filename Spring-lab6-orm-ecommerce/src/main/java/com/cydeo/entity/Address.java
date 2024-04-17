af004a32c13dbaac9438ada22140ad8797c0df9f
Kadifa <Kudickadifa@gmail.com>
2024-04-16 22:04:45 -0500

    practice

--- Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Address.java
+++ Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Address.java
@@ -1,4 +1,22 @@
 package com.cydeo.entity;
 
-public class Address {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Address extends BaseEntity{
+
+
+    private String name;
+    private String street;
+    private String zipCode;
+    @ManyToOne
+    private Customer customerId;
+
+
 }
---
+++ Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Balance.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Balance extends BaseEntity {
+
+
+    private BigDecimal amount;
+    @OneToOne
+    private Customer customerId;
+
+}
