53f5e233160a88736c78fa469da9d0213802af58
Kadifa <Kudickadifa@gmail.com>
2024-04-17 05:58:10 -0500

    product category rel

--- Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Product.java
+++ Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Product.java
@@ -1,11 +1,15 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,6 +21,16 @@ public class Product extends BaseEntity{
     private BigDecimal price;
     private Integer quantity;
     private Integer remainingQuantity;
+    // in product side will have less load when bring category
+    // we are writing relationship in product entity
+    @ManyToMany
+    @JoinTable(name = "product_category_rel",
+                joinColumns = @JoinColumn(name = "p_id"),
+                inverseJoinColumns = @JoinColumn(name = "c_id"))
+    private List<Category> categoryList;
+
+
+
 
 
 }
