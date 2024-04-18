25ba3ebf19366c8c4ea6e48cd5ad90155d0201ec
Kadifa <Kudickadifa@gmail.com>
2024-04-17 21:35:50 -0500

    product category rel

---
+++ Spring-lab6-orm-ecommerce/src/main/java/com/cydeo/entity/Order.java
@@ -0,0 +1,29 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "orders")
+public class Order extends BaseEntity{
+
+    private BigDecimal paidPrice;
+    private BigDecimal totalPrice;
+    @OneToOne
+    private Cart cart;
+    @ManyToOne
+    private Customer customer;
+    @OneToOne
+    private Payment payment;
+
+}
