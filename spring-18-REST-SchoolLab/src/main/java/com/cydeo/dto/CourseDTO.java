1d8ec11a05e4a4db3e84b38e350147953507825c
Kadifa <Kudickadifa@gmail.com>
2024-05-26 08:04:17 -0500

    REST api practice

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -0,0 +1,16 @@
+package com.cydeo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseDTO {
+    private Long id;
+    private String courseName;
+    private String description;
+}
