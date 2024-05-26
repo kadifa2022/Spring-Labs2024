c0ae175d72aa68e5d1500bdee2813f3f50ecd114
Kadifa <Kudickadifa@gmail.com>
2024-05-26 08:04:46 -0500

    REST api practice

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/TeacherDTO.java
@@ -0,0 +1,29 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.EducationLevel;
+import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherDTO {
+
+    private Long id;
+    private String firstName;
+    private String phoneNumber;
+
+    private String email;
+    private String username;
+    private String password;
+    private LocalDate birthday;
+    private Status status;
+    private EducationLevel educationLevel;
+    private AddressDTO addressDTO;
+}
---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/entity/Teacher.java
@@ -0,0 +1,41 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.EducationLevel;
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "teacher")
+public class Teacher extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    private String email;
+    private String username;
+    private String password;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private EducationLevel educationLevel;
+
+    private Address address;
+
+}
