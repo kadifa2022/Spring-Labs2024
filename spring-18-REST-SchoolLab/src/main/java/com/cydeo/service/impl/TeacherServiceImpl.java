fe2367e94412bac66027b5068d240b9394d5dccb
Kadifa <Kudickadifa@gmail.com>
2024-05-27 06:59:45 -0500

    REST api practice

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/TeacherServiceImpl.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/TeacherServiceImpl.java
@@ -1,23 +1,32 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.repository.TeacherRepository;
 import com.cydeo.service.TeacherService;
+import com.cydeo.utill.MapperUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
 
-    private final ModelMapper modelMapper;
+    private final MapperUtil mapperUtil;
+    private final TeacherRepository teacherRepository;
 
-    public TeacherServiceImpl(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
+    public TeacherServiceImpl( MapperUtil mapperUtil, TeacherRepository teacherRepository) {
+        this.mapperUtil = mapperUtil;
+
+        this.teacherRepository = teacherRepository;
     }
 
     @Override
     public List<TeacherDTO> findAll() {
-        return null;
+        return teacherRepository.findAll()
+                .stream()
+                .map(teacher -> mapperUtil.convert(teacher, new TeacherDTO()))
+                .collect(Collectors.toList());
     }
 }
