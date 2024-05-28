3fcd28553adce92deec616592d7bb3f8f31d4f37
Kadifa <Kudickadifa@gmail.com>
2024-05-28 05:28:20 -0500

    REST api practice

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
@@ -2,6 +2,9 @@ package com.cydeo.dto;
 
 import com.cydeo.enums.AddressType;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressDTO {
 
+    @JsonIgnore
     private Long id;
+
     private String street;
     private String country;
     private String state;
@@ -21,11 +28,13 @@ public class AddressDTO {
     private String postalCode;
 
     private AddressType addressType;
-
+    // if we have more than one back reference, Jason will be confused that's why we add
+    //names structure to each object
+    @JsonBackReference(value = "student-address-reference")
     private StudentDTO student;
-
+    @JsonBackReference(value = "parent-address-reference")
     private ParentDTO parent;
-    @JsonBackReference
+    @JsonBackReference(value = "teacher-address-reference")
     private TeacherDTO teacher;
 
     private Integer currentTemperature;
