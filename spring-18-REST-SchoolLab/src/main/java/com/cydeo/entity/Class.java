7c285b2aecf4be49bab0e1320a045b622e99a105
Kadifa <Kudickadifa@gmail.com>
2024-06-02 21:43:51 -0500

    REST api practice

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/entity/Class.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "class")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Class extends BaseEntity{
+    private String name;
+    private Integer year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+}
--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
@@ -1,30 +1,44 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.client.WeatherApiClient;
 import com.cydeo.dto.AddressDTO;
+import com.cydeo.dto.weather.WeatherDTO;
 import com.cydeo.entity.Address;
 import com.cydeo.repository.AddressRepository;
 import com.cydeo.service.AddressService;
 import com.cydeo.utill.MapperUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
+
+
+
     private final AddressRepository addressRepository;
     private final MapperUtil mapperUtil;
+    private final WeatherApiClient weatherApiClient;
 
-    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil) {
+    @Value("${access_key}")
+    private String access_key;
+
+    public AddressServiceImpl(AddressRepository addressRepository, MapperUtil mapperUtil, WeatherApiClient weatherApiClient) {
+
         this.addressRepository = addressRepository;
         this.mapperUtil = mapperUtil;
+        this.weatherApiClient = weatherApiClient;
     }
 
 
     @Override
     public List<AddressDTO> findAll() {
         return addressRepository.findAll()
-                .stream().map(address -> mapperUtil.convert(address, new AddressDTO()))
+                .stream()
+                .map(address -> mapperUtil.convert(address, new AddressDTO()))
                 .collect(Collectors.toList());
     }
 
@@ -33,16 +47,45 @@ public class AddressServiceImpl implements AddressService {
         Address foundAddress = addressRepository.findById(id)
                 .orElseThrow(()-> new Exception("No Address Found!"));
         AddressDTO addressDTO = mapperUtil.convert(foundAddress, new AddressDTO());
+        //we will get current temperature and set based on city, and return DTO
+       // addressDTO.setCurrentTemperature(retrieveCurrentWeather(addressDTO.getCity()).getCurrent().getTemperature());
+        addressDTO.setCurrentTemperature(retrieveTemperatureByCity(addressDTO.getCity()));
         return addressDTO;
     }
 
+    private Integer retrieveTemperatureByCity(String city) {
+        return weatherApiClient.getCurrentWeather(access_key, city).getCurrent().getTemperature();
+    }
+
+    private WeatherDTO retrieveCurrentWeather(String city) {
+        return weatherApiClient.getCurrentWeather(access_key, city);
+
+
+    }
+
+
     @Override
-    public AddressDTO update(AddressDTO addressDTO) {
-        return null;
+    public AddressDTO update(AddressDTO addressDTO) throws Exception {
+        addressRepository.findById(addressDTO.getId())
+                .orElseThrow(()->new Exception("No Address Found"));
+        Address addressToSave = mapperUtil.convert(addressDTO, new Address());
+
+        addressRepository.save(addressToSave);
+        AddressDTO updateAddress = mapperUtil.convert(addressToSave, new AddressDTO());
+
+        return updateAddress;
     }
 
     @Override
-    public AddressDTO create(AddressDTO addressDTO) {
-        return null;
+    public AddressDTO create(AddressDTO addressDTO) throws Exception {
+        Optional<Address> foundAddress = addressRepository.findById(addressDTO.getId());
+        if(foundAddress.isPresent()){
+            throw new Exception("Address Already Exists!");
+        }
+        Address addressToSave =mapperUtil.convert(addressDTO, new Address());
+        addressRepository.save(addressToSave);
+
+        return mapperUtil.convert(addressToSave, new AddressDTO());
     }
+
 }
