aa06eeba2b34b5c545a240d4845ffdb330fd99ce
Kadifa <Kudickadifa@gmail.com>
2024-06-02 21:46:40 -0500

    REST api practice

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/StudentDTO.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/StudentDTO.java
@@ -1,6 +1,7 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.Status;
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,11 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Setter
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudentDTO {
+
+    @JsonIgnore
     private Long id;
     private String firstName;
     private String phoneNumber;
@@ -20,10 +25,13 @@ public class StudentDTO {
     private String email;
     private String username;
 
-    private String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;//we need to se password,but we don't need to show
     private LocalDate birthday;
     private Status status;
+    @JsonManagedReference(value = "student-address-reference")
     private AddressDTO address;
+
     private ParentDTO parent;
 
 }
