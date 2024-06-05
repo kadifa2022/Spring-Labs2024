581c1b12f1c0ea316a8f3c7fa20651e883fb2589
Kadifa <Kudickadifa@gmail.com>
2024-06-05 10:41:25 -0500

    REST api practice

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/client/CountryApiClient.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/client/CountryApiClient.java
@@ -1,17 +1,20 @@
 package com.cydeo.client;
 
+import com.cydeo.dto.country.CountryDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.websocket.server.PathParam;
+import java.util.List;
+
 
 @FeignClient(url = "https://restcountries.com/v3.1", name = "COUNTRY-CLIENT")
 public interface CountryApiClient {
     ///https://restcountries.com/v3.1/name/Finland
 
-    @GetMapping("/name/{countryName}")
-    Object getCountryInfo(@PathVariable("countryName") String countryName);
+    @GetMapping("/name/{countryName}") // if we have error Json cannot deserialize (from Array value)
+    // return type must be List<CountryDTO> // if we're returning list than structures needs to change in Service impl.to (get(0)) get the first element from the list
+    List<CountryDTO> getCountryInfo(@PathVariable("countryName") String countryName);
 
 }
 
