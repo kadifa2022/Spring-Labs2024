96ac352900b8c16a193ce2cd6a808d4ba15f79f2
Kadifa <Kudickadifa@gmail.com>
2024-06-05 21:50:46 -0500

    REST api practice

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Category.java
@@ -0,0 +1,14 @@
+package com.cydeo.lab08rest.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Category extends BaseEntity {
+
+    private String name;
+}
