0c09c2b9a464a5376de1f240c7c020decc7218bd
Kadifa <Kudickadifa@gmail.com>
2024-06-05 19:44:41 -0500

    REST api practice

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab08rest.service.impl;
+
+import com.cydeo.lab08rest.entity.Category;
+import com.cydeo.lab08rest.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public Category findById(Long categoryId) {
+        return null;
+    }
+}
