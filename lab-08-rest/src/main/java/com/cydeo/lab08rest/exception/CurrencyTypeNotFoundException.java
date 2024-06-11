8d12919ecd172a3c769f59b000dfcfafb5d4e4c0
Kadifa <Kudickadifa@gmail.com>
2024-06-11 05:51:30 -0500

    REST api practice

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/exception/CurrencyTypeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cydeo.lab08rest.exception;
+
+public class CurrencyTypeNotFoundException extends NotFoundException {
+    public CurrencyTypeNotFoundException(String message) {
+        super(message);
+    }
+}
